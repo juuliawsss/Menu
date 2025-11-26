92a6ca8463dadc6cc817b511ba6797e04b7bc1e3
juuliawsss <juuliawylipelkonen@gmail.com>
2025-11-26 12:44:32 +0200

    s

---
+++ Assets/Scripts/Dessert.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class Dessert : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

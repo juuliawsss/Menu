982608fd5703c6030d76a4765e26b6b9b82fd33b
juuliawsss <juuliawylipelkonen@gmail.com>
2025-12-03 13:35:02 +0200

    Mains

--- Assets/Scripts/Mains.cs
+++ Assets/Scripts/Mains.cs
@@ -1,16 +1,23 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Mains : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        // ...existing code...
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        // ...existing code...
+    }
+
+    // Call this from a UI button to load the Mains scene
+    public void LoadMainsScene()
+    {
+        SceneManager.LoadScene("Mains");
     }
 }

b74b2f044f38703510ffabf36073810191a22df4
juuliawsss <juuliawylipelkonen@gmail.com>
2025-12-10 09:45:37 +0200

    v

--- Assets/Scripts/Dessert.cs
+++ Assets/Scripts/Dessert.cs
@@ -2,15 +2,46 @@ using UnityEngine;
 
 public class Dessert : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    // Reference to the Shoppingcart object (assign in Inspector)
+    public GameObject shoppingcart;
+
+    private string pannacotta = "Pannacotta - Vaniljalla maustettua kermavanukasta ja granaattiomenakastiketta. (Gluteeniton, Laktoositon) 9.00€";
+    private string kahvi = "Aito italialaistyylinen kahvi 2.50€";
+    private string juustokakku = "Italialainen tuorejuustokakku 10.00€";
+    private string gelato = "Gelato - Italialainen jäätelö, mansikka 6.00€";
+
+    // Call these from UI buttons for each dessert
+    public void OrderPannacotta()
     {
-        
+        AddDessertToCart(pannacotta);
+    }
+    public void OrderKahvi()
+    {
+        AddDessertToCart(kahvi);
+    }
+    public void OrderJuustokakku()
+    {
+        AddDessertToCart(juustokakku);
+    }
+    public void OrderGelato()
+    {
+        AddDessertToCart(gelato);
     }
 
-    // Update is called once per frame
-    void Update()
+    private void AddDessertToCart(string dessert)
     {
-        
+        if (shoppingcart != null)
+        {
+            Shoppingcart cartScript = shoppingcart.GetComponent<Shoppingcart>();
+            if (cartScript != null)
+            {
+                cartScript.AddItem(dessert);
+                Debug.Log($"Added dessert: {dessert}");
+            }
+        }
+        else
+        {
+            Debug.LogWarning("Shoppingcart reference not set in Dessert.cs");
+        }
     }
 }

302ac22405d838fd6961daa793a7a6d658b881f4
juuliawsss <juuliawylipelkonen@gmail.com>
2025-12-15 23:19:52 +0200

    v

--- Assets/Scripts/DrinksDropdown.cs
+++ Assets/Scripts/DrinksDropdown.cs
@@ -1,44 +1,95 @@
 using UnityEngine;
 using TMPro;
 using System.Collections.Generic;
+using System.Globalization;
 
 public class DrinksDropdown : MonoBehaviour
 {
     [Header("UI")]
-    [SerializeField] private TMP_Dropdown dropdown;
+    [SerializeField] private TMP_Dropdown drinksDropdown;
+    [SerializeField] private TMP_Dropdown amountDropdown;
+    [SerializeField] private TMP_Text totalText;
 
-    private List<string> drinks = new List<string>
+    private Dictionary<int, float> drinkPrices = new Dictionary<int, float>
     {
-        "Valitse juoma",
-        "Punaviini - 10.00€",
-        "Valkoviini - 10.00€",
-        "Peroni-olut - 8.00€",
-        "Cola - 3.00€"
+        { 1, 10.00f }, // Punaviini
+        { 2, 10.00f }, // Valkoviini
+        { 3, 8.00f },  // Peroni
+        { 4, 3.00f }   // Cola
     };
 
     void Start()
     {
-        if (dropdown == null)
-            dropdown = GetComponent<TMP_Dropdown>();
+        SetupDrinksDropdown();
+        SetupAmountDropdown();
 
-        dropdown.ClearOptions();
-        dropdown.AddOptions(drinks);
+        drinksDropdown.onValueChanged.AddListener(_ => UpdateTotal());
+        amountDropdown.onValueChanged.AddListener(_ => UpdateTotal());
 
-        dropdown.value = 0;
-        dropdown.RefreshShownValue();
-
-        dropdown.onValueChanged.AddListener(OnDrinkSelected);
+        UpdateTotal();
     }
 
-    private void OnDrinkSelected(int index)
+    private void SetupDrinksDropdown()
     {
-        if (index == 0) return; // "Valitse juoma"
+        drinksDropdown.ClearOptions();
+        drinksDropdown.AddOptions(new List<string>
+        {
+            "Valitse juoma",
+            "Punaviini - 10.00€",
+            "Valkoviini - 10.00€",
+            "Peroni-olut - 8.00€",
+            "Cola - 3.00€"
+        });
 
-        string selectedDrink = dropdown.options[index].text;
+        drinksDropdown.value = 0;
+    }
 
-        Debug.Log("Selected drink: " + selectedDrink);
+    private void SetupAmountDropdown()
+    {
+        amountDropdown.ClearOptions();
+        amountDropdown.AddOptions(new List<string>
+        {
+            "x1",
+            "x2",
+            "x3",
+            "x4",
+            "x5"
+        });
 
-        // Lähetetään OrderSummaryyn / ostoskoriin
-        OrderDropdown.SetCurrentMenuItem(selectedDrink);
+        amountDropdown.value = 0;
+    }
+
+    private void UpdateTotal()
+    {
+        int drinkIndex = drinksDropdown.value;
+        int amount = amountDropdown.value + 1;
+
+        if (drinkIndex == 0 || !drinkPrices.ContainsKey(drinkIndex))
+        {
+            totalText.text = "Total: 0.00€";
+            return;
+        }
+
+        float total = drinkPrices[drinkIndex] * amount;
+        totalText.text = $"Total: {total.ToString("F2", CultureInfo.InvariantCulture)}€";
+
+        Debug.Log($"Selected: {drinksDropdown.options[drinkIndex].text} x{amount} = {total}€");
+    }
+
+    // Kutsu tätä nappulasta: "Lisää ostoskoriin"
+    public void AddToCart()
+    {
+        int drinkIndex = drinksDropdown.value;
+        if (drinkIndex == 0) return;
+
+        int amount = amountDropdown.value + 1;
+        string drinkName = drinksDropdown.options[drinkIndex].text;
+        float total = drinkPrices[drinkIndex] * amount;
+
+        string item = $"{drinkName} x{amount} - {total:F2}€";
+
+        Shoppingcart.Instance.AddItem(item);
+        Debug.Log("Added to cart: " + item);
     }
 }
+

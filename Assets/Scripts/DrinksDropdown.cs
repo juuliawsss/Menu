e3ca8ab4f7a65a7d47dac88c4870be5ae8cb20d6
juuliawsss <juuliawylipelkonen@gmail.com>
2025-12-15 23:38:23 +0200

    a

--- Assets/Scripts/DrinksDropdown.cs
+++ Assets/Scripts/DrinksDropdown.cs
@@ -1,98 +1,35 @@
 using UnityEngine;
-using TMPro;
-using System.Collections.Generic;
-using System.Globalization;
 
 public class DrinksDropdown : MonoBehaviour
 {
-    private TMP_Dropdown drinksDropdown;
-    private TMP_Dropdown amountDropdown;
-    private TMP_Text totalText;
-
-    private Dictionary<int, float> drinkPrices = new Dictionary<int, float>
+    // Public methods for each drink to be called from UI (TextMeshPro or Button)
+    public void OnPunaviiniClicked()
     {
-        { 1, 10.00f }, // Punaviini
-        { 2, 10.00f }, // Valkoviini
-        { 3, 8.00f },  // Peroni
-        { 4, 3.00f }   // Cola
-    };
-
-    void Start()
-    {
-        // Etsitään komponentit automaattisesti
-        drinksDropdown = GetComponent<TMP_Dropdown>();
-        amountDropdown = GetComponent<TMP_Dropdown>();
-
-    totalText = FindFirstObjectByType<TMP_Text>();
-
-        SetupDrinksDropdown();
-        SetupAmountDropdown();
-
-        drinksDropdown.onValueChanged.AddListener(_ => UpdateTotal());
-        amountDropdown.onValueChanged.AddListener(_ => UpdateTotal());
-
-        UpdateTotal();
+        AddDrinkToCart("Punaviini - 10.00€");
     }
 
-    private void SetupDrinksDropdown()
+    public void OnValkoviiniClicked()
     {
-        drinksDropdown.ClearOptions();
-        drinksDropdown.AddOptions(new List<string>
-        {
-            "Valitse juoma",
-            "Punaviini - 10.00€",
-            "Valkoviini - 10.00€",
-            "Peroni-olut - 8.00€",
-            "Cola - 3.00€"
-        });
-
-        drinksDropdown.value = 0;
-        drinksDropdown.RefreshShownValue();
+        AddDrinkToCart("Valkoviini - 10.00€");
     }
 
-    private void SetupAmountDropdown()
+    public void OnPeroniClicked()
     {
-        amountDropdown.ClearOptions();
-        amountDropdown.AddOptions(new List<string>
-        {
-            "x1", "x2", "x3", "x4", "x5"
-        });
-
-        amountDropdown.value = 0;
-        amountDropdown.RefreshShownValue();
+        AddDrinkToCart("Peroni-olut - 8.00€");
     }
 
-    private void UpdateTotal()
+    public void OnColaClicked()
     {
-        int drinkIndex = drinksDropdown.value;
-        int amount = amountDropdown.value + 1;
-
-        if (drinkIndex == 0 || !drinkPrices.ContainsKey(drinkIndex))
-        {
-            totalText.text = "Total: 0.00€";
-            return;
-        }
-
-        float total = drinkPrices[drinkIndex] * amount;
-        totalText.text = $"Total: {total.ToString("F2", CultureInfo.InvariantCulture)}€";
-
-        Debug.Log($"{drinksDropdown.options[drinkIndex].text} x{amount} = {total:F2}€");
+        AddDrinkToCart("Cola - 3.00€");
     }
 
-    // Kutsu tätä nappulasta
-    public void AddToCart()
+    // Helper to add a drink to the cart (one at a time)
+    private void AddDrinkToCart(string drink)
     {
-        int drinkIndex = drinksDropdown.value;
-        if (drinkIndex == 0) return;
-
-        int amount = amountDropdown.value + 1;
-        float total = drinkPrices[drinkIndex] * amount;
-
-        string item =
-            $"{drinksDropdown.options[drinkIndex].text} x{amount} - {total:F2}€";
-
+        int selectedAmount = 1; // Default to 1, or get from your UI if needed
+        string item = $"{drink} x{selectedAmount}";
         Shoppingcart.Instance.AddItem(item);
-        Debug.Log("Added to cart: " + item);
+        Debug.Log($"Added drink to cart: {item}");
     }
 }
 
